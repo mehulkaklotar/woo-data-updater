80fed9fe91a7ac461d87274e40f82e48e1ce3586
mehulkaklotar <mehul.kaklotar@gmail.com>
2015-09-25 00:44:33 +0530

    initial plugin file

---
+++ woo-data-updater.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Plugin Name: Woo Data Updater
+ * Plugin URI: https://github.com/mehulkaklotar/woo-data-updater
+ * Description: A woocommerce add on to support data updater with WP-CLI
+ * Version: 1.0
+ * Author: Mehul Kaklotar
+ * Author URI: http://mehulkaklotar.branded.me
+ * Requires at least: 4.0
+ * Tested up to: 4.3
+ *
+ * Text Domain: woo-data-updater
+ *
+ * @package Woo_Data_Updater
+ * @category Core
+ * @author mehulkaklotar
+ */
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly
+}
